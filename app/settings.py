512e54f0d74c03c451cbe0797880570bf60445e2
Ivan Kalita <ivkalita@domclick.ru>
2018-03-17 15:23:34 +0300

    Add DRF pagination.

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,8 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100
 }
 
 ROOT_URLCONF = 'app.urls'
